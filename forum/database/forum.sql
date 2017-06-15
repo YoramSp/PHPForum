bd13ceb1f09c1a8cefbb25f91ac15773c96d9e0f
Yoram <spee_yoram@live.nl>
2017-06-15 20:37:28 +0200

    Huidig database
    Database voor het testen van het forum. Uiteindelijke database komt nog.

---
+++ forum/database/forum.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 15 jun 2017 om 20:36
+-- Serverversie: 10.1.16-MariaDB
+-- PHP-versie: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `forum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `replies`
+--
+
+CREATE TABLE `replies` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `content` longtext NOT NULL,
+  `created-at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated-at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `topic_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `replies`
+--
+
+INSERT INTO `replies` (`ID`, `content`, `created-at`, `updated-at`, `topic_id`, `user_id`) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2017-05-23 09:52:02', '0000-00-00 00:00:00', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `threads`
+--
+
+CREATE TABLE `threads` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `subject` varchar(20) NOT NULL,
+  `description` longtext NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `created-at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated-at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `topics`
+--
+
+CREATE TABLE `topics` (
+  `ID` int(10) UNSIGNED NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `content` longtext NOT NULL,
+  `created-at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated-at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `thread_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(5) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `surname` varchar(25) NOT NULL,
+  `birthdate` date NOT NULL,
+  `first_login` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`ID`, `username`, `password`, `email`, `name`, `surname`, `birthdate`, `first_login`, `last_login`) VALUES
+(6, 'Yoram', '2637a5c30af69a7bad877fdb65fbd78b', 'spee_yoram@live.nl', 'Yoram', 'Spee', '1993-08-19', '2017-06-15 19:51:02', '0000-00-00 00:00:00');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexen voor tabel `threads`
+--
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexen voor tabel `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT voor een tabel `threads`
+--
+ALTER TABLE `threads`
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
